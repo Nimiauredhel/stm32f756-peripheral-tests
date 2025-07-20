f2ee693b2056caa3805891f4dc311e49998dcc9a
Mickey Hoz <nimiauredhel@gmail.com>
2025-07-20 19:14:13 +0300

    implemented sending of the "test started" confirmation

--- f756-peripheral-tests-server/App/test_runner.c
+++ f756-peripheral-tests-server/App/test_runner.c
@@ -29,17 +29,21 @@ void test_runner_task_init(void)
 
 void test_runner_task_loop(void)
 {
+	static const uint16_t test_runner_delay_ticks = (pdMS_TO_TICKS(250));
+
 	static char debug_buff[256] = {0};
 
 	for(;;)
 	{
-		vTaskDelay(pdMS_TO_TICKS(500));
+		vTaskDelay(test_runner_delay_ticks);
 
 		if (osOK == osMessageQueueGet(TestQueueHandle, &current_test, 0, HAL_MAX_DELAY))
 		{
 			serial_debug_enqueue("Test Runner executing requested test.");
 
-			// prepare test reference
+			/**
+			 * Preparing the test reference variables.
+			 */
 			test_string_len = current_test.request[TEST_PACKET_STRING_LEN_OFFSET];
 			explicit_bzero(test_string_buff, sizeof(test_string_buff));
 			strncpy(test_string_buff, (char *)(current_test.request+TEST_PACKET_STRING_HEAD_OFFSET), test_string_len);
@@ -49,15 +53,20 @@ void test_runner_task_loop(void)
 			uint8_t ordered_test_count = 0;
 			uint8_t completed_tests = 0;
 
+			/**
+			 * Signaling that the tests should start running
+			 * by updating the test data structs.
+			 */
 			for (uint8_t i = 0; i < NUM_POSSIBLE_TESTS; i++)
 			{
 				if (0x01 & (test_selection_byte >> (uint8_t)i))
 				{
-					sprintf(debug_buff, "%s Test Ordered.", test_defs[i].name);
-					serial_debug_enqueue(debug_buff);
 					ordered_test_count++;
 					test_defs[i].iterations = current_test.request[TEST_PACKET_ITERATIONS_BYTE_OFFSET];
 					test_defs[i].state = TESTSTATE_PENDING;
+
+					snprintf(debug_buff, sizeof(debug_buff), "%s Test Ordered.", test_defs[i].name);
+					serial_debug_enqueue(debug_buff);
 				}
 				else
 				{
@@ -65,20 +74,30 @@ void test_runner_task_loop(void)
 				}
 			}
 
-			// prepare results packet
-			// using request packet as a base
+			/**
+			 * Preparing and sending a "test started" confirmation packet,
+			 * based on the currently processed "new test" request packet.
+			 */
+			explicit_bzero(&message_scratch, sizeof(message_scratch));
 			message_scratch.addr = current_test.client_addr;
 			message_scratch.port = current_test.client_port;
 			memcpy(message_scratch.message, current_test.request, TEST_MSG_PACKET_SIZE_BYTES);
+			message_scratch.message[TEST_PACKET_MSG_BYTE_OFFSET] = TESTMSG_TEST_START_ACK;
+			message_scratch.message[TEST_PACKET_SELECTION_BYTE_OFFSET] = 0x01;
+			message_scratch.message[TEST_PACKET_ITERATIONS_BYTE_OFFSET] = TEST_PACKET_END_BYTE_VALUE;
+			osMessageQueuePut(OutboxQueueHandle, &message_scratch, 0, HAL_MAX_DELAY);
+
+			/**
+			 * Preparing the base "test results" packet.
+			 */
 			message_scratch.message[TEST_PACKET_MSG_BYTE_OFFSET] = TESTMSG_TEST_OVER_RESULTS;
 			message_scratch.message[TEST_PACKET_SELECTION_BYTE_OFFSET] = 0x00;
-			message_scratch.message[TEST_PACKET_ITERATIONS_BYTE_OFFSET] = TEST_PACKET_END_BYTE_VALUE;
 
-			serial_debug_enqueue("Awaiting test completion.");
+			serial_debug_enqueue("Test Runner awaiting test completion.");
 
 			while(completed_tests < ordered_test_count)
 			{
-				vTaskDelay(pdMS_TO_TICKS(250));
+				vTaskDelay(test_runner_delay_ticks);
 
 				for (uint8_t i = 0; i < NUM_POSSIBLE_TESTS; i++)
 				{
@@ -87,13 +106,13 @@ void test_runner_task_loop(void)
 				case TESTSTATE_SUCCESS:
 					  message_scratch.message[TEST_PACKET_SELECTION_BYTE_OFFSET]
 						  |= (1 << (uint8_t)i);
-					  sprintf(debug_buff, "%s Test Success.", test_defs[i].name);
+					  snprintf(debug_buff, sizeof(debug_buff), "%s Test Success.", test_defs[i].name);
 					  serial_debug_enqueue(debug_buff);
 					  test_defs[i].state = TESTSTATE_READY;
 					  completed_tests++;
 					  break;
 				case TESTSTATE_FAILURE:
-					  sprintf(debug_buff, "%s Test Failure.", test_defs[i].name);
+					  snprintf(debug_buff, sizeof(debug_buff), "%s Test Failure.", test_defs[i].name);
 					  serial_debug_enqueue(debug_buff);
 					  test_defs[i].state = TESTSTATE_READY;
 					  completed_tests++;
